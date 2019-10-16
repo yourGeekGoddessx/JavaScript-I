fbe87fca1af49b74e74185c58594d8972da929eb
yourgeekgoddess <yourgeekgoddessx@gmail.com>
2019-10-15 23:30:28 -0400

    done arrow functions :revolving_hearts:

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,21 +4,37 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+const one = () => console.log("Function was invoked!");
 
+one();
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const two = (param) => {return param;}
+
+console.log (two("this is a string"))
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const three = (param1, param2) => { return param1 + param2;}
+
+console.log(three(1,2))
+
+
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+const four = (param1, param2) => { return param1 - param2;}
+
+console.log(four(1,2))
 
 
 // Stretch
